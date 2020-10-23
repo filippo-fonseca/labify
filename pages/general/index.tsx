b03e1bc2d440d11b6c60bc83d16660c17fb0f6e1
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-10-23 08:34:19 -0600

    feat(general): Add general

---
+++ pages/general/index.tsx
@@ -0,0 +1,95 @@
+import Home from "../../components/Home";
+
+const General = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        padding: "15px",
+      }}
+      className="general-bck"
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          height: "765px",
+          width: "90%",
+          textAlign: "center",
+          marginLeft: "auto",
+          marginRight: "auto",
+          borderRadius: "15px",
+          padding: "15px",
+        }}
+        className="general-box"
+      >
+        <h1>General</h1>
+        <p>
+          <strong>Goal:</strong> To determine if the height impacts the final
+          velocity of an object in free fall.{" "}
+        </p>
+        <p>
+          <strong>Independent Variable:</strong> Height of the Object (m)
+          Dependent Variable: Final Velocity (m/s)
+        </p>
+        <p style={{ fontSize: "24px" }}>
+          <strong>Equation:</strong> a = (Vf - Vi) / t
+        </p>
+        <img
+          height="200px"
+          src="https://upload.wikimedia.org/wikipedia/en/1/1e/Baseball_%28crop%29.jpg"
+        ></img>
+        <img
+          height="200px"
+          src="https://miro.medium.com/max/500/1*cPh7ujRIfcHAy4kW2ADGOw.png"
+        ></img>
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              textAlign: "justify",
+              marginLeft: "50px",
+              maxWidth: "45%",
+            }}
+          >
+            <h2>Story</h2>
+            <p>
+              I've always loved building things; in fact, it's my primarily goal
+              in life. Specifically, I thrive best when I'm presented with the
+              challenge of building tools that actually help others and serve
+              them with true value. So when I was presented with the opportunity
+              to do so for this lab experiment (along with Mark Music), I was
+              all for it. Not only that, but going above and beyond to create
+              comething that could potentially help others understand a certain
+              subject (in this case gravitational physics) in a better way is
+              something I will never say no to.
+            </p>
+          </div>
+
+          <div
+            style={{
+              textAlign: "justify",
+              marginLeft: "50px",
+              maxWidth: "45%",
+            }}
+          >
+            <h2>Methodology</h2>
+            <p>
+              Besides executing the experiment to its completeness, Mark and I
+              decided to take it one step further. Our entire project comprises
+              this very web app, a beautifully made video, and the required lab
+              report as well. More specifically, the tech stack utilized for
+              this app includes the following:{" "}
+              <ul>
+                <li>TypeScript</li>
+                <li>React.js</li>
+                <li>Next.js</li>
+                <li>HTML and CSS principles</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default General;
